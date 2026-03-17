3b1db688449e3da81c818e7c5ae6768c7c082e34
IshimweArlene <ishimwearlene74@gmail.com>
2026-03-17 21:50:30 +0200

    Update Next.js to 16.1.7 to fix security vulnerability and update dependencies

--- src/app/breakfast/page.tsx
+++ src/app/breakfast/page.tsx
@@ -2,7 +2,7 @@
 import TopBar from "../components/topBar";
 import SearchNav from "../components/searchBar";
 import MealCard from "../components/mealCard";
-import { meals } from "../meals/data";
+import { breakFast } from "../meals/break";
 
 const BreakFast = () => {
     return (
@@ -12,7 +12,7 @@ const BreakFast = () => {
               <SearchNav />
                <div className="px-8 py-4 overflow-y-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-7xl mx-auto">
-                  {meals.map((meal, i) => (
+                  {breakFast.map((meal, i) => (
                       <MealCard
                       key={i}
                       name={meal.name}
